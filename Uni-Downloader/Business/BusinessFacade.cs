d0e3847d975fe8ab92ce628686f4c674ed850940
Ruben Rodrigues <ruben2000@gmail.com>
2021-10-04 16:20:23 +0100

    Home Screen Done

---
+++ Uni-Downloader/Business/BusinessFacade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Business.Services;
+
+namespace Business
+{
+	public class BusinessFacade
+	{
+		private BusinessFacade()
+		{
+		}
+
+		public static BusinessFacade Instance { get; } = new();
+
+
+		public string GetBaseUniversityDirectory() => DirectoriesService.Instance.UniToBaseDirectory;
+
+		public string GetDownloadsDirectory() => DirectoriesService.Instance.UniFromDirectory;
+
+		public void SetBaseUniversityDirectory(string baseUniDirectory) =>
+			DirectoriesService.Instance.UniToBaseDirectory = baseUniDirectory;
+
+		public void SetDownloadsDirectory(string downloadsDirectory) =>
+			DirectoriesService.Instance.UniFromDirectory = downloadsDirectory;
+		public void SaveChanges()
+		{
+			DirectoriesService.Instance.SaveChanges();
+		}
+
+		public void LoadDatabase()=>DirectoriesService.Instance.SaveChanges();
+	}
+}
